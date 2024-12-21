9c2b35ab911d9589b763fe39e41000e27b48e434
MohammadKardi1 <mohammadkardi1@gmail.com>
2024-12-21 20:56:03 +0100

    update

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+
+
+
+const app = express()
+
+
+dotenv.config()
+
+
+
+const PORT = process.env.PORT || 3000 
+const CONNECTION_URL = process.env.MONGO_URL
+
+
+// ['http://localhost:5173', 'https://medicare-client-mocha.vercel.app'] 
+// Middleware
+app.use(cors({
+    credentials: true,
+    origin: true
+}))
+app.use(cookieParser())
+app.use(express.json())
+
+
+// Routes for endpoints
+// app.use('/api/auth', )
+// app.use('/api/user', ) 
+// app.use('/api/product', )
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "API is running"})
+})
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => {
+        console.log(error.message)
+    })
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+})
